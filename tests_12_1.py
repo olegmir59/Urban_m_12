ca67670d79d5f0cb76695f225d17486e80a24453
olegmir59 <udom1@ya.ru>
2024-10-17 13:36:40 +0700

    Modify file

--- tests_12_1.py
+++ tests_12_1.py
@@ -2,14 +2,33 @@
 # Цель: приобрести навык создания простейших Юнит-тестов
 #
 # Задача "Проверка на выносливость":
+
 import unittest
+import runner
 
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
-        pass
+        walk_1 = runner.Runner("Bob")
+        for i in range(10):
+            walk_1.walk()
+        self.assertEqual(walk_1.distance, 50)
+
 
     def test_run(self):
-        pass
+        run_1 = runner.Runner("Peter")
+        for i in range(10):
+            run_1.run()
+        self.assertEqual(run_1.distance, 100)
+
     def test_challenge(self):
-        pass
+        walk_1 = runner.Runner("Bob")
+        run_1 = runner.Runner("Peter")
+        for i in range(10):
+            walk_1.walk()
+            run_1.run()
+        self.assertNotEqual(run_1.distance, walk_1.distance)
+
+
+if __name__ == '__main__':
+    unittest.main

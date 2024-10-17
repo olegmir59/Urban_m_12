d2ea22f7b99ba6e3364aa7646a5616aaefed917b
olegmir59 <udom1@ya.ru>
2024-10-17 12:41:48 +0700

    Add files

---
+++ tests_12_1.py
@@ -0,0 +1,15 @@
+# Домашнее задание по теме "Простые Юнит-Тесты"
+# Цель: приобрести навык создания простейших Юнит-тестов
+#
+# Задача "Проверка на выносливость":
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        pass
+
+    def test_run(self):
+        pass
+    def test_challenge(self):
+        pass
